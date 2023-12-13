2ef3ac5c15c0bf984c573ddd2b559264c198a584
jabir254 <jabirkullow@gmail.com>
2023-12-13 21:55:41 +0300

    added register, login, get user routers

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const authController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+//register a new user
+router.post("/regiser", authController.registerUser);
+
+//authenticate and log in user
+router.post("/login", authController.loginUser);
+
+//get authenticated user
+router.get(
+  "/me",
+  authMiddleware.authenticateUser,
+  authController.getAuthenticatedUser
+);
+
+module.exports = router;

aa6f7f36dc436d42c41b1b72a46cf96a73d6f4a4
jabir254 <jabirkullow@gmail.com>
2023-12-13 21:25:11 +0300

    removed password hashing moved to auth controller

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -7,27 +7,5 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-userSchema.pre("save", async function (next) {
-  const user = this;
-
-  if (user.isModified("password") || user.isNew) {
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(user.password, salt);
-      user.password = hash;
-      next();
-    } catch (error) {
-      return next(error);
-    }
-  } else {
-    return next();
-  }
-});
-
-//compare password
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
-};
-
 const User = mongoose.model("User", userSchema);
 module.exports = User;

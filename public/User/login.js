bacf75b1e03a16e2d333979988bbf833397a0612
jabir254 <jabirkullow@gmail.com>
2023-12-16 21:10:10 +0300

    added form id and changed fetch method link to ful

--- public/User/login.js
+++ public/User/login.js
@@ -1,51 +1,46 @@
-function signUp() {
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+// Function to handle user registration
+async function registerUser() {
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-    fetch('/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: username,
-        email: email,
-        password: password,
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-        // Handle success, e.g., redirect to another page
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        // Handle error, e.g., display an error message to the user
-      });
+  const response = await fetch('localhost:3000/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, email, password }),
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    alert('Registration successful!');
+    console.log(data);
+  } else {
+    alert(`Registration failed: ${data.error}`);
   }
+}
 
-  function login() {
-    const loginUsername = document.getElementById('loginUsername').value;
-    const loginPassword = document.getElementById('loginPassword').value;
+// Function to handle user login
+async function loginUser() {
+  const loginUsername = document.getElementById('loginUsername').value;
+  const loginPassword = document.getElementById('loginPassword').value;
 
-    fetch('/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username:loginUsername,
-        password: loginPassword,
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-        // Handle success, e.g., set user authentication token
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        // Handle error, e.g., display an error message to the user
-      });
+  const response = await fetch('/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: loginUsername, password: loginPassword }),
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    alert('Login successful!');
+    console.log(data);
+  } else {
+    alert(`Login failed: ${data.error}`);
   }
+}

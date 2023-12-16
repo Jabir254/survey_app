105f6e3e42339188074e4f2c6fd4ee61e8ec025c
jabir254 <jabirkullow@gmail.com>
2023-12-16 21:24:40 +0300

    updated

--- public/User/login.js
+++ public/User/login.js
@@ -1,13 +1,13 @@
 // Function to handle user registration
 async function registerUser() {
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-  const response = await fetch('localhost:3000/auth/register', {
-    method: 'POST',
+  const response = await fetch("localhost:3000/auth/register", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ username, email, password }),
   });
@@ -15,7 +15,7 @@ async function registerUser() {
   const data = await response.json();
 
   if (response.ok) {
-    alert('Registration successful!');
+    alert("Registration successful!");
     console.log(data);
   } else {
     alert(`Registration failed: ${data.error}`);
@@ -24,13 +24,13 @@ async function registerUser() {
 
 // Function to handle user login
 async function loginUser() {
-  const loginUsername = document.getElementById('loginUsername').value;
-  const loginPassword = document.getElementById('loginPassword').value;
+  const loginUsername = document.getElementById("username").value;
+  const loginPassword = document.getElementById("password").value;
 
-  const response = await fetch('/auth/login', {
-    method: 'POST',
+  const response = await fetch("/auth/login", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ username: loginUsername, password: loginPassword }),
   });
@@ -38,7 +38,7 @@ async function loginUser() {
   const data = await response.json();
 
   if (response.ok) {
-    alert('Login successful!');
+    alert("Login successful!");
     console.log(data);
   } else {
     alert(`Login failed: ${data.error}`);

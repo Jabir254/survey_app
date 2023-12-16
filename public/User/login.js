2653ad8f98c592f9907367ae56af18f8e7ac5483
jabir254 <jabirkullow@gmail.com>
2023-12-16 21:26:42 +0300

    updated using axios cdn

--- public/User/login.js
+++ public/User/login.js
@@ -1,46 +1,39 @@
+// Import Axios library
+import axios from 'axios';
+
 // Function to handle user registration
 async function registerUser() {
-  const username = document.getElementById("username").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-  const response = await fetch("localhost:3000/auth/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, email, password }),
-  });
+  try {
+    // Make a POST request using Axios
+    const response = await axios.post('/register', { username, email, password });
 
-  const data = await response.json();
-
-  if (response.ok) {
-    alert("Registration successful!");
-    console.log(data);
-  } else {
-    alert(`Registration failed: ${data.error}`);
+    // Handle the response
+    alert('Registration successful!');
+    console.log(response.data);
+  } catch (error) {
+    // Handle errors
+    alert(`Registration failed: ${error.response.data.error}`);
   }
 }
 
 // Function to handle user login
 async function loginUser() {
-  const loginUsername = document.getElementById("username").value;
-  const loginPassword = document.getElementById("password").value;
+  const loginUsername = document.getElementById('username').value;
+  const loginPassword = document.getElementById('password').value;
 
-  const response = await fetch("/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username: loginUsername, password: loginPassword }),
-  });
+  try {
+    // Make a POST request using Axios
+    const response = await axios.post('/login', { username: loginUsername, password: loginPassword });
 
-  const data = await response.json();
-
-  if (response.ok) {
-    alert("Login successful!");
-    console.log(data);
-  } else {
-    alert(`Login failed: ${data.error}`);
+    // Handle the response
+    alert('Login successful!');
+    console.log(response.data);
+  } catch (error) {
+    // Handle errors
+    alert(`Login failed: ${error.response.data.error}`);
   }
 }

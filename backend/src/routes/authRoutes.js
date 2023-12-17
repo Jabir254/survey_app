2a9e95ee972df5681f0e39fdce97079e77b03b95
jabir254 <jabirkullow@gmail.com>
2023-12-17 12:36:15 +0300

    updated and removed errors

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
+const path = require("path");
 
 const router = express.Router();
 
